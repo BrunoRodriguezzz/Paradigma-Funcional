1dfe79585fb414fe829cd9b87f066596b304da6f
Bruno Rodriguez <brrodriguez@frba.utn.edu.ar>
2024-04-14 20:02:00 -0300

    EjercicoNomus compila

--- EjercicioNomus.hs
+++ EjercicioNomus.hs
@@ -1,38 +1,38 @@
-data Nomus = UnNomus{
-    tieneAlas:: Bool,
-    multiplesBrazos:: Bool,
-    cantOjos:: Int,
-    color:: String,
-    vida:: Int,
-    fuerza:: Int,
-    poderes:: [unPoder]
-}
+data Nomus = UnNomus
+  { tieneAlas :: Bool,
+    multiplesBrazos :: Bool,
+    cantOjos :: Int,
+    color :: String,
+    vida :: Int,
+    fuerza :: Int,
+    poderes :: [Poder]  }
 
---Parte 1
+-- Parte 1
 
-puedeVer:: Nomus -> Bool
-puedeVer (unNomus alas brazos ojos color vida fuerza [Poder]) = ojos >= 1 
+puedeVer :: Nomus -> Bool
+puedeVer (UnNomus _ _ ojos _ _ _ _) = ojos >= 1
 
-determinarCategoria:: Nomus -> String
-determinarCategoria (unNomus alas brazos ojos color vida fuerza [Poder]) | fuerza >= 10000 = "high-end"
-                                                                 | fuerza >= 3000 = "fuerte"
-                                                                 | fuerza >= 1000 = "comun"
-                                                                 | otherwise = "pichi"
+determinarCategoria :: Nomus -> String
+determinarCategoria (UnNomus _ _ _ _ _ fuerza _)
+  | fuerza >= 10000 = "high-end"
+  | fuerza >= 3000 = "fuerte"
+  | fuerza >= 1000 = "comun"
+  | otherwise = "pichi"
 
---Parte 2
+-- Parte 2
 
-data Poder = UnPoder{
-    probabilidadCritico:: Float
-    curacionUso:: Int,
-    cantDaño:: Int,
-    rango:: Int,
-}
+data Poder = UnPoder
+  { probabilidadCritico :: Float,
+    curacionUso :: Int,
+    cantDaño :: Int,
+    rango :: Int
+  }
 
-probCriticoUltPoder:: Nomus -> Float
-probabilidadCritico (unNomus a, b, o, c, v, f, [Poder]) = probabilidadCriticoPoder . extraerUltPoder
+probCriticoUltPoder :: Nomus -> Float
+probCriticoUltPoder (UnNomus _ _ _ _ _ _ poderes) = probabilidadCriticoPoder (extraerUltPoder poderes)
 
-probabilidadCriticoPoder:: Poder -> Float
-probabilidadCriticoPoder (unPoder probCritico _ _ _) = probCritico
+probabilidadCriticoPoder :: Poder -> Float
+probabilidadCriticoPoder (UnPoder probCritico _ _ _) = probCritico
 
-extraerUltPoder:: [Poder] -> Poder
-extraerUltPoder [x] = last [x] 
+extraerUltPoder :: [Poder] -> Poder
+extraerUltPoder [x] = last [x]
--- TP1SueldosDocentes.hs
+++ TP1SueldosDocentes.hs
@@ -1,19 +1,22 @@
 sueldo :: String -> Float -> Float -> Float
-sueldo x y z = basicoPorCargo x * antiguedad y * fromIntegral (cantidadHoras z)  
+sueldo x y z = basicoPorCargo x * antiguedad y * fromIntegral (cantidadHoras z)
 
 basicoPorCargo :: String -> Float
-basicoPorCargo x | x == "titular" = 149000
-                 | x == "adjunto" = 116000
-                 | x == "ayudante" = 66000
-                 | otherwise = 0
+basicoPorCargo x
+  | x == "titular" = 149000
+  | x == "adjunto" = 116000
+  | x == "ayudante" = 66000
+  | otherwise = 0
 
 antiguedad :: Float -> Float
-antiguedad x | x >= 24 = 2.2
-             | x >= 10 = 1.5
-             | x >= 5  = 1.3
-             | x >= 3  = 1.2
-             | otherwise = 1
+antiguedad x
+  | x >= 24 = 2.2
+  | x >= 10 = 1.5
+  | x >= 5 = 1.3
+  | x >= 3 = 1.2
+  | otherwise = 1
 
 cantidadHoras :: Float -> Int
-cantidadHoras x | x < 50 && x > 0 = round (x/10)
-                | otherwise = 0 
+cantidadHoras x
+  | x < 50 && x > 0 = round (x / 10)
+  | otherwise = 0
