e527ccc534a6bd12deda1eba19ce05a8fc64fdc4
BrunoRodriguezzz <128913461+BrunoRodriguezzz@users.noreply.github.com>
2024-05-24 15:09:07 -0300

    Delete Ejercicios  directory

--- Ejercicios /Ejercicio_inicial_Paradigma_Funcional-Pinos.hs
+++
@@ -1,9 +0,0 @@
-pesoPino :: Float -> Float
-pesoPino x | x <= 3 = x*100*3
-           | otherwise = 900 + (x-3)*100*2
-
-esPesoUtil :: Float -> Bool
-esPesoUtil x = 400 <= x && x <= 1000
-
-esPinoUtil :: Float -> Bool
-esPinoUtil = esPesoUtil . pesoPino 
